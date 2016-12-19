2430c23fdc576b35ea0aae2ff63058e236f1d108
Alastair Irvine <alastair@plug.org.au>
2016-12-20 01:15:19 +0800

    Minimal directory structure and config
    composer.json needs to be modified to set namespaces
    
    composer.lock is empty
    
    docroot/index.php doesn't do anything
    
    .gitignore ignores swap/backup files and vendor/

---
+++ bin/demo.php
@@ -0,0 +1,13 @@
+#! /usr/bin/php
+<?php
+  require_once dirname(__DIR__) . '/vendor/autoload.php';
+?>
+Demo!
+<?php
+
+
+# vim: set tabstop=2 shiftwidth=2 :
+# Local Variables:
+# tab-width: 2
+# end:
+?>
---
+++ docroot/index.php
@@ -0,0 +1,21 @@
+<?php
+  require_once dirname($_SERVER["DOCUMENT_ROOT"]) . '/vendor/autoload.php';
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+<?php
+?>
+  </body>
+</html>
+<?php
+
+
+# vim: set tabstop=2 shiftwidth=2 :
+# Local Variables:
+# tab-width: 2
+# end:
+?>
